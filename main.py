f31d1a06d2de88a010918634300328dc4c3925f7
Mike Zubko <mishau7@gmail.com>
2022-08-03 05:14:48 +0300

    Initial commit

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import morse_converter
 
 print(morse_converter.encode('Hello, World!'))
 print(morse_converter.decode('.... . .-.. .-.. --- --..-- / .-- --- .-. .-.. -.. -.-.--'))
-
+#
 morse_converter.main()
